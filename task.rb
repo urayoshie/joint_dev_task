eb7673c40b2dc3d5db34133b8dacbbcc6950245b
urayoshie <nof.q.p1417@gmail.com>
2020-12-08 02:12:28 +0900

    q17_commit

--- task.rb
+++ task.rb
@@ -160,7 +160,19 @@ end
 
 class UserQ17
   # 以下に回答を記載
+  attr_accessor :name, :age, :gender
 
+  def initialize(name:, age:, gender:)
+    @name = name
+    @age = age
+    @gender = gender
+  end
+
+  def info
+    puts "名前: #{@name}"
+    puts "年齢: #{@age}"
+    puts "性別: #{@gender}"
+  end
 end
 
 def q17
